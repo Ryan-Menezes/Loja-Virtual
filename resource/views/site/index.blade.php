e0081aacfd86f1df46bdaea8936c1c53430a823d
Ryan Menezes <menezesryan1010@gmail.com>
2022-03-16 15:38:16 -0300

    Alterando página home

--- resource/views/site/index.blade.php
+++ resource/views/site/index.blade.php
@@ -174,52 +174,6 @@
 </div>
 <!-- /SECTION -->
 
-<!-- HOT DEAL SECTION -->
-<div id="hot-deal" class="section">
-	<!-- container -->
-	<div class="container">
-		<!-- row -->
-		<div class="row">
-			<div class="col-md-12">
-				<div class="hot-deal">
-					<ul class="hot-deal-countdown">
-						<li>
-							<div>
-								<h3>02</h3>
-								<span>Days</span>
-							</div>
-						</li>
-						<li>
-							<div>
-								<h3>10</h3>
-								<span>Hours</span>
-							</div>
-						</li>
-						<li>
-							<div>
-								<h3>34</h3>
-								<span>Mins</span>
-							</div>
-						</li>
-						<li>
-							<div>
-								<h3>60</h3>
-								<span>Secs</span>
-							</div>
-						</li>
-					</ul>
-					<h2 class="text-uppercase">hot deal this week</h2>
-					<p>New Collection Up to 50% OFF</p>
-					<a class="primary-btn cta-btn" href="#">Shop now</a>
-				</div>
-			</div>
-		</div>
-		<!-- /row -->
-	</div>
-	<!-- /container -->
-</div>
-<!-- /HOT DEAL SECTION -->
-
 <!-- SECTION -->
 <div class="section">
 	<!-- container -->
