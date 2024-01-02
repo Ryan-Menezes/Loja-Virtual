57e0de6c31677c4a7a1e832ed2f8fed60686dca9
Ryan de Menezes <menezesryan1010@gmail.com>
2024-01-02 16:12:48 -0300

    Adicionando um campo para digitar a página desejada na paginação

--- resource/views/includes/paginator.blade.php
+++ resource/views/includes/paginator.blade.php
@@ -6,41 +6,48 @@
 @endphp
 
 @if($pages > 1)
-<nav class="m-0">
-	<ul class="pagination m-0">
-		@if($currentPage !== 1)
-			<li class="page-item"><a class="page-link" href="?page=1" title="Primeira página">Primeiro</a></li>
-		@endif
+<div style="display: flex; gap: 20px;">
+    <nav class="m-0">
+        <ul class="pagination m-0">
+            @if($currentPage !== 1)
+                <li class="page-item"><a class="page-link" href="?page=1" title="Primeira página">Primeiro</a></li>
+            @endif
 
-		@if(empty($builder))
-			@for($i = $currentPage - $limitPage; $i < $currentPage && $i <= $limitPageItems; $i++)
-			 	@if($i > 0)
-					<li class="page-item"><a class="page-link" href="?page={{ $i }}" title="Página {{ $i }}">{{ $i }}</a></li>
-				@endif
-			@endfor
-        @else
-        	@for($i = $currentPage - $limitPage; $i < $currentPage && $i <= $limitPageItems; $i++)
-			 	@if($i > 0)
-					<li class="page-item"><a class="page-link" href="?page={{ $i }}&{{ http_build_query($builder) }}" title="Página {{ $i }}">{{ $i }}</a></li>
-				@endif
-			@endfor
-        @endif
+            @if(empty($builder))
+                @for($i = $currentPage - $limitPage; $i < $currentPage && $i <= $limitPageItems; $i++)
+                    @if($i > 0)
+                        <li class="page-item"><a class="page-link" href="?page={{ $i }}" title="Página {{ $i }}">{{ $i }}</a></li>
+                    @endif
+                @endfor
+            @else
+                @for($i = $currentPage - $limitPage; $i < $currentPage && $i <= $limitPageItems; $i++)
+                    @if($i > 0)
+                        <li class="page-item"><a class="page-link" href="?page={{ $i }}&{{ http_build_query($builder) }}" title="Página {{ $i }}">{{ $i }}</a></li>
+                    @endif
+                @endfor
+            @endif
 
-		<li class="page-item active"><a class="page-link" href="#" title="Página {{ $currentPage }}">{{ $currentPage }}</a></li>
+            <li class="page-item active"><a class="page-link" href="#" title="Página {{ $currentPage }}">{{ $currentPage }}</a></li>
 
-		@if(empty($builder))
-			@for($i = $currentPage + 1; $i <= $pages && $i <= $limitPageItems; $i++)
-			 	<li class="page-item"><a class="page-link" href="?page={{ $i }}" title="Página {{ $i }}">{{ $i }}</a></li>
-            @endfor
-        @else
-        	@for($i = $currentPage + 1; $i <= $pages && $i <= $limitPageItems; $i++)
-			 	<li class="page-item"><a class="page-link" href="?page={{ $i }}&{{ http_build_query($builder) }}" title="Página {{ $i }}">{{ $i }}</a></li>
-            @endfor
-        @endif
+            @if(empty($builder))
+                @for($i = $currentPage + 1; $i <= $pages && $i <= $limitPageItems; $i++)
+                    <li class="page-item"><a class="page-link" href="?page={{ $i }}" title="Página {{ $i }}">{{ $i }}</a></li>
+                @endfor
+            @else
+                @for($i = $currentPage + 1; $i <= $pages && $i <= $limitPageItems; $i++)
+                    <li class="page-item"><a class="page-link" href="?page={{ $i }}&{{ http_build_query($builder) }}" title="Página {{ $i }}">{{ $i }}</a></li>
+                @endfor
+            @endif
 
-		@if($currentPage !== $pages)
-			<li class="page-item"><a class="page-link" href="?page={{$pages}}" title="Última página">Último</a></li>
-		@endif
-	</ul>
-</nav>
+            @if($currentPage !== $pages)
+                <li class="page-item"><a class="page-link" href="?page={{$pages}}" title="Última página">Último</a></li>
+            @endif
+        </ul>
+    </nav>
+
+    <form method="GET" style="width: 150px; display: flex; gap: 5px;">
+        <input type="number" name="page" placeholder="Página" class="form-control" value="{{ $currentPage }}" />
+        <button type="submit" class="btn btn-primary">IR</button>
+    </form>
+</div>
 @endif
